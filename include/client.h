184379745789f2616275f1b354d62b7c11d22e5a
Luchooooo <louis.barreteau@epitech.eu>
2023-03-28 16:48:01 +0200

    feat(core): help option for client and server and upgrade makefile to clear all object

---
+++ include/client.h
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2023
+** client
+** File description:
+** In this file for the client
+*/
+
+#pragma once
---
+++ include/server.h
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2023
+** server
+** File description:
+** In this file for the server
+*/
+
+#pragma once
