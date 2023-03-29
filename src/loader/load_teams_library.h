6ef4aefd770e3d737ddd983a6ef9a4eef67cf1fa
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-03-29 11:02:42 +0200

    feat(loader): add load_teams_library function template, loading all functions into an array of functions pointers

---
+++ src/loader/load_teams_library.h
@@ -0,0 +1,9 @@
+/*
+** EPITECH PROJECT, 2023
+** NWP_myteams_2023 [WSL : Ubuntu]
+** File description:
+** load_teams_library
+*/
+
+#pragma once
+
---
+++ src/loader/main.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** NWP_myteams_2023 [WSL : Ubuntu]
+** File description:
+** main
+*/
+
+#include "load_teams_library.h"
+
+int main(int argc, char **argv)
+{
+    load_teams_library(argv[1]);
+}
