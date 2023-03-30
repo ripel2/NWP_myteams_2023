a5a01d2eceadf06b1e4a4e4b77329e89b5f5f925
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-03-30 11:01:48 +0200

    feat(loader): add more tests for comparing the output got with the output desired

--- src/server/main.c
+++ src/server/main.c
@@ -8,9 +8,32 @@
 #include <stdio.h>
 #include <string.h>
 #include "shared.h"
+#include "loader.h"
+#include <unistd.h>
+
+DLLoader_t *init_dll(void)
+{
+    DLLoader_t *dll = malloc(sizeof(DLLoader_t));
+
+    load_teams_library("libs/myteams/libmyteams.so", dll);
+    return dll;
+}
+
+void fini_dll(DLLoader_t *dll)
+{
+    dlclose(dll->handle);
+    free(dll);
+}
 
 int main(int ac, char **av)
 {
-    print_help_if_needed(ac, av);
+    DLLoader_t *dll = init_dll();
+
+    (void)(av);
+    (void)(ac);
+
+    dll->functions[CLIENT_CHANNEL_PRINT_THREADS]("a", "a", 1, "a", "a");
+
+    fini_dll(dll);
     return 0;
 }
