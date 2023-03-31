f183cb823dac99c511cc42b8d82a72004153ddc5
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-03-31 19:48:32 +0200

    feat(loader): fix norm in print_help.c file made by louis barreteau

--- src/shared/print_help.c
+++ src/shared/print_help.c
@@ -45,7 +45,7 @@ bool print_help_if_needed(int ac, char **av)
             print_client_help();
         return true;
     }
-    if (av[1] != NULL && (strcmp(av[1], "-help") == 0 \
+    if (av[1] != NULL && (strcmp(av[1], "-help") == 0
         || strcmp(av[1], "--help") == 0 || strcmp(av[1], "-h") == 0)) {
         if (what_type == SERVER)
             print_server_help();
