875d7d537aea2ddce9e41306ed2319f8839f14d7
Luchooooo <louis.barreteau@epitech.eu>
2023-03-31 12:12:37 +0200

    fix(core): print if he have two args in server and client

--- src/print_help.c
+++ src/print_help.c
@@ -38,12 +38,13 @@ bool print_help_if_needed(int ac, char **av)
     enum { SERVER, CLIENT, ERROR };
     unsigned short what_type = get_type(av);
 
-    if (ac < 2 || ac > 3) {
-        if (what_type == SERVER)
-            print_server_help();
-        if (what_type == CLIENT)
-            print_client_help();
+    if (what_type == SERVER && ac != 2) {
+        print_server_help();
         return true;
+    }
+    if (what_type == CLIENT && ac != 3) {
+        print_client_help();
+        return false;
     }
     if (av[1] != NULL && (strcmp(av[1], "-help") == 0 \
         || strcmp(av[1], "--help") == 0 || strcmp(av[1], "-h") == 0)) {
