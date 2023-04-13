6301a0ebfd707d237aebdf42ed47a4bb84174c8a
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-04-13 21:33:14 +0200

    fix: norm error in .h files (too long lines and no backline end of file

--- include/data_struct_functions.h
+++ include/data_struct_functions.h
@@ -14,8 +14,8 @@ user_t *get_user_from_struct(const char *uuid);
 int add_team_to_struct(const char *name, const char *uuid,
 const char *description);
 team_t *get_team_from_struct(const char *uuid);
-int add_channel_to_struct(const char *team_uuid, const char *name, const char *uuid,
-const char *description);
+int add_channel_to_struct(const char *team_uuid, const char *name,
+const char *uuid, const char *description);
 channel_t *get_channel_from_struct(const char *uuid);
 
 char *generate_uuid(char *uuid);
