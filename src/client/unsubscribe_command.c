bc6a1f7665990601ede2bd524b0490cbf0e86a04
Lucas Hauszler <lucas.hauszler@epitech.eu>
2023-04-15 12:36:51 +0200

    feat(client): add unsubscribe command

--- src/client/unsubscribe_command.c
+++ src/client/unsubscribe_command.c
@@ -14,10 +14,29 @@
 #include "client.h"
 #include "client_functions.h"
 
+static int unsubscribe_command_end(line_t *answer)
+{
+    write(1, answer->buf, answer->len);
+    free(answer->buf);
+    free(answer);
+    return 0;
+}
+
 int unsubscribe_command(client_t *client, client_info_t *info, char **args)
 {
-    (void)client;
-    (void)args;
+    char command[128] = {0};
+    line_t *line = NULL;
+    int ret = 0;
+
     (void)info;
-    return 0;
+    strcpy(command, "UNSUBSCRIBE");
+    if (args[1] != NULL) {
+        strcat(command, " ");
+        strcat(command, args[1]);
+        strcat(command, "\n");
+    }
+    ret = execute_simple_command(client, command, strlen(command), &line);
+    if (ret != 0)
+        return ret;
+    return subscribe_command_end(line);
 }
