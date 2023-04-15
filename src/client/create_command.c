febe8385dd4a0764eedfa937021ac4aced2afc55
Lucas Hauszler <lucas.hauszler@epitech.eu>
2023-04-15 17:26:06 +0200

    feat(client): add create command

--- src/client/create_command.c
+++ src/client/create_command.c
@@ -14,10 +14,58 @@
 #include "client.h"
 #include "client_functions.h"
 
+static int create_command_cleanup(client_t *client)
+{
+    line_t *line = NULL;
+
+    client_get_line_timeout(client, &line);
+    if (line == NULL)
+        return -1;
+    write(1, line->buf, line->len);
+    free(line->buf);
+    free(line);
+    return 0;
+}
+
+static int create_command_end(client_t *client, line_t *line, char *msg)
+{
+    int ret = 0;
+    char *msgline = NULL;
+
+    if (line->len > 0 && line->buf[0] != '3') {
+        write(1, line->buf, line->len);
+        return 0;
+    }
+    msgline = strtok(msg, "\n");
+    while (msgline != NULL) {
+        ret = client_write_string(client, msgline);
+        if (ret != 0)
+            return ret;
+        ret = client_write_string(client, "\n");
+        if (ret != 0)
+            return ret;
+        msgline = strtok(NULL, "\n");
+    }
+    return create_command_cleanup(client);
+}
+
 int create_command(client_t *client, client_info_t *info, char **args)
 {
-    (void)client;
-    (void)args;
+    char command[1024] = {0};
+    line_t *line = NULL;
+    int ret = 0;
+
     (void)info;
-    return 0;
+    strcpy(command, "CREATE");
+    if (args[1] != NULL && args[2] != NULL) {
+        strcat(command, " ");
+        strcat(command, args[1]);
+        strcat(command, " ");
+        strcat(command, "1");
+    }
+    strcat(command, "\n");
+    ret = execute_simple_command(client, command, strlen(command), &line);
+    if (ret != 0)
+        return ret;
+    return create_command_end(client, line, args[2]);
 }
