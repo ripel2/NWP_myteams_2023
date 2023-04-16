52127387275fb7f90fbaf091a165ab4523b15db0
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-04-16 18:42:45 +0200

    feat(is_channel_in_team): function for verifying if a channel beyong to a team or not

--- src/server/handle_database/other_functions/is_channel_in_team.c
+++ src/server/handle_database/other_functions/is_channel_in_team.c
@@ -11,17 +11,17 @@
 #include <string.h>
 #include <unistd.h>
 
-int is_channel_in_team(const char *channel_uuid, const char *team_uuid)
+bool is_channel_in_team(const char *channel_uuid, const char *team_uuid)
 {
     team_t *team = get_team_from_struct(team_uuid);
     channel_t *channel;
 
     if (team == NULL || get_channel_from_struct(channel_uuid) == NULL)
-        return (84);
+        return (false);
     TAILQ_FOREACH(channel, &team->channels, entries) {
         if (channel && strcmp(channel->channel_data->uuid, channel_uuid) == 0) {
-            return (0);
+            return (true);
         }
     }
-    return (84);
+    return (false);
 }
