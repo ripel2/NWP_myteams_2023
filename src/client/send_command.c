e945615c7c7a9256bfb49ab0563a9f1332105667
Lucas Hauszler <lucas.hauszler@epitech.eu>
2023-04-16 10:58:48 +0200

    feat(client): add debug calls to send

--- src/client/send_command.c
+++ src/client/send_command.c
@@ -14,7 +14,17 @@
 #include "client.h"
 #include "client_functions.h"
 
-static int send_command_cleanup(client_t *client)
+static void send_command_error_debug(line_t *line, char **args)
+{
+    if (strncmp(line->buf, "530", 3) == 0)
+        dll.functions[CLIENT_ERROR_UNAUTHORIZED]();
+    else if (strncmp(line->buf, "430", 3) == 0 ||
+    strncmp(line->buf, "550", 3) == 0)
+        dll.functions[CLIENT_ERROR_UNKNOWN_USER](args[1]);
+}
+
+static int send_command_cleanup(client_t *client, char **args,
+client_info_t *info)
 {
     line_t *line = NULL;
 
@@ -22,21 +32,26 @@ static int send_command_cleanup(client_t *client)
     if (line == NULL)
         return -1;
     write(1, line->buf, line->len);
+    if (strncmp(line->buf, "200", 3) == 0)
+        dll.functions[CLIENT_EVENT_PRIVATE_MESSAGE_RECEIVED](info->username,
+        args[2]);
     free(line->buf);
     free(line);
     return 0;
 }
 
-static int send_command_end(client_t *client, line_t *line, char *msg)
+static int send_command_end(client_t *client, line_t *line, char **args,
+client_info_t *info)
 {
     int ret = 0;
     char *msgline = NULL;
 
     if (line->len > 0 && line->buf[0] != '3') {
         write(1, line->buf, line->len);
+        send_command_error_debug(line, args);
         return 0;
     }
-    msgline = strtok(msg, "\n");
+    msgline = strtok(args[2], "\n");
     while (msgline != NULL) {
         ret = client_write_string(client, msgline);
         if (ret != 0)
@@ -46,7 +61,7 @@ static int send_command_end(client_t *client, line_t *line, char *msg)
             return ret;
         msgline = strtok(NULL, "\n");
     }
-    return send_command_cleanup(client);
+    return send_command_cleanup(client, args, info);
 }
 
 int send_command(client_t *client, client_info_t *info, char **args)
@@ -67,5 +82,5 @@ int send_command(client_t *client, client_info_t *info, char **args)
     ret = execute_simple_command(client, command, strlen(command), &line);
     if (ret != 0)
         return ret;
-    return send_command_end(client, line, args[2]);
+    return send_command_end(client, line, args, info);
 }
