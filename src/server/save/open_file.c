9e169da927374f9b0f2afbbf30e3c4836b109174
Luchooooo <louis.barreteau@epitech.eu>
2023-04-18 15:05:57 +0200

    fix(core): change perror by write

--- src/server/save/open_file.c
+++ src/server/save/open_file.c
@@ -14,20 +14,20 @@ FILE *open_file(const char *path, const char *mode)
     FILE *fd = NULL;
 
     if (path == NULL || mode == NULL) {
-        perror("Error the path or the mode is NULL");
+        write(1, "Error: path or mode is NULL", 27);
         return NULL;
     }
     if (access(path, F_OK) == -1) {
         fd = fopen(path, "w");
         if (fd == NULL) {
-            perror("Error creating the file");
+            write(1, "Error: can't create the file", 28);
             return NULL;
         }
         fclose(fd);
     }
     fd = fopen(path, mode);
     if (fd == NULL) {
-        perror("Error opening the file");
+        write(1, "Error: can't open the file", 26);
         return NULL;
     }
     return fd;
