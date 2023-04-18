29db0f82cbf91242bc7df627fbb1a6469b7f6168
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-04-18 18:15:45 +0200

    fix: add function declaration in shared.h

--- include/shared.h
+++ include/shared.h
@@ -12,3 +12,4 @@
 bool print_help_if_needed(int ac, char **av);
 bool is_port_valid(const char *port);
 bool is_ip_valid(const char *ip);
+void split_string_fixed_array(char *str, char **args, size_t size);
