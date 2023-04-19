c5d2b6699f5f1953d80e286e698c0b2587600221
Lucas Hauszler <lucas.hauszler@epitech.eu>
2023-04-20 00:03:05 +0200

    chore: push real help command

--- src/client/commands/help_command.c
+++ src/client/commands/help_command.c
@@ -8,11 +8,25 @@
 #include "client.h"
 #include "client_functions.h"
 
+static int help_print_answer(char *answer)
+{
+    puts("MyTeams");
+    puts("Server supported commands:");
+    puts(answer);
+    return 0;
+}
+
 int help_command(client_t *client, char **args)
 {
-    (void)client;
+    int ret = 0;
+    char line[32768] = {0};
+
     (void)args;
-    printf("Pour gagner, il ne faut pas perdre.\n");
-    printf("\t- Karim Benzema\n");
+    client_printf(client, "HELP\n");
+    ret = client_read_in_buffer(client);
+    if (ret != 0)
+        return ret;
+    if (client_flush_line(client, line))
+        help_print_answer(line);
     return 0;
 }
