e825bd1698c2a81dace992f9413d16492294dccb
Luchooooo <louis.barreteau@epitech.eu>
2023-04-19 16:04:10 +0200

    feat(core): user command

--- src/server/commands/user_command.c
+++ src/server/commands/user_command.c
@@ -11,6 +11,7 @@
 #include "shared.h"
 #include "data_struct_functions.h"
 #include "teams_server.h"
+#include "teams_commands.h"
 #include "data.h"
 #include "server.h"
 
@@ -18,16 +19,24 @@ void handle_user(server_t *server, server_client_t *client, char **args)
 {
     (void)server;
     (void)client;
-    server_client_write_string(server, client, "Command: ");
-    server_client_write_string(server, client, args[0]);
-    server_client_write_string(server, client, "\n");
-    server_client_write_string(server, client, "Arguments: ");
+    user_t *user = NULL;
+
     if (args[1] == NULL) {
         server_client_write_string(server, client, "No arguments given\n");
         return;
     }
-    for (int i = 1; args[i]; i++) {
-        server_client_write_string(server, client, args[i]);
-        server_client_write_string(server, client, " ");
+    remove_bad_char(args[1]);
+    string_strip_delim(&args[1], '"');
+    user = get_user_from_struct(args[1]);
+    if (user == NULL) {
+        server_client_write_string(server, client, "430 User doesn't exist\n");
+        return;
     }
+    server_client_write_string(server, client, "150 ");
+    server_client_write_string(server, client, user->user_data->uuid);
+    server_client_write_string(server, client, " ");
+    server_client_write_string(server, client, user->user_data->name);
+    server_client_write_string(server, client, " ");
+    server_client_write_string(server, client, (user->is_logged ? "1" : "0"));
+    server_client_write_string(server, client, "\n");
 }
