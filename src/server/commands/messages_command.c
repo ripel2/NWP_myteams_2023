46375ceee34fc1ead66edd275ef28cfb1e32e69a
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-04-19 23:28:21 +0200

    feat(create_messages_command): fix norm error in all files (too long lines)

--- src/server/commands/messages_command.c
+++ src/server/commands/messages_command.c
@@ -53,19 +53,6 @@ user_t *user_to_seek, server_t *server, server_client_t *client)
     }
 }
 
-static int count_nb_messages(user_t *current_user, user_t *user_to_seek)
-{
-    personal_discussion_t *discussion =
-    get_discussion_between_user(current_user, user_to_seek);
-    int nb_messages = 0;
-    message_t *message = NULL;
-
-    TAILQ_FOREACH(message, &discussion->messages, entries) {
-        nb_messages++;
-    }
-    return nb_messages;
-}
-
 static void list_all_message_between_user(user_t *current_user,
 user_t *user_to_seek
 , server_t *server, server_client_t *client)
@@ -82,28 +69,36 @@ user_t *user_to_seek
     server_client_write_string(server, client, "200 OK\n");
 }
 
+static bool is_args_error(server_t *server, server_client_t *client
+, user_t *current_user, char **args)
+{
+    if (current_user == NULL) {
+        server_client_write_string(server, client, "530 Not logged in\n");
+        return true;
+    } else if (args[1] == NULL || args[2] != NULL) {
+        server_client_write_string(server, client, "432 Invalid arguments\n");
+        return true;
+    }
+    return false;
+}
+
 void handle_messages(server_t *server, server_client_t *client, char **args)
 {
     user_t *user_to_seek = NULL;
     user_t *current_user = NULL;
 
     current_user = get_user_logged_in(client);
-    if (current_user == NULL) {
-        server_client_write_string(server, client, "530 Not logged in\n");
+    if (is_args_error(server, client, current_user, args) == true)
         return;
-    } else if (args[1] == NULL || args[2] != NULL) {
-        server_client_write_string(server, client, "432 Invalid arguments\n");
-        return;
-    }
     string_strip_delim(&args[1], '"');
     remove_bad_char(args[1]);
     if (is_a_uuid(args[1]) == false) {
-        server_client_write_string(server, client, "431 Invalid UUID\n");
+        server_client_write_string(server, client, "550 Bad uuid\n");
         return;
     }
     user_to_seek = get_user_from_struct(args[1]);
     if (user_to_seek == NULL) {
-        server_client_write_string(server, client, "550 Bad uuid\n");
+        server_client_write_string(server, client, "430 User doesn't exist\n");
         return;
     }
     list_all_message_between_user(current_user, user_to_seek, server, client);
