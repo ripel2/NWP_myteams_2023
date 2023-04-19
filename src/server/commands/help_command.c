0ae66ee22dad16596ed18d4a175a1fe94d3486e1
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-04-19 17:55:13 +0200

    feat(create_help_command): bad display of the command

--- src/server/commands/help_command.c
+++ src/server/commands/help_command.c
@@ -17,10 +17,9 @@
 void handle_help(server_t *server, server_client_t *client, char **args)
 {
     if (args[1] == NULL) {
-        server_client_write_string(server, client, "214 myTeams commands: \n\t"
-        "help\n\tlogin\n\tlogout\n\tusers\n\tuser\n\tsend\n\tmessages\n\tsubsc"
-        "ribe\n\tsubscribed\n\tunsubscribe\n\tuse"
-        "\n\tcreate\n\tlist\n\tinfo\n");
+        server_client_write_string(server, client, "214 214 HELP LOGIN "
+        "LOGOUT USERS USER SEND MESSAGES SUBSCRIBE SUBSCRIBED UNSUBSCRIBE"
+        " USETEAM USECHANNEL USETHREAD CREATE LIST INFO\n");
     } else {
         server_client_write_string(server, client,
         "501 Syntax error in parameters or arguments.\n");
