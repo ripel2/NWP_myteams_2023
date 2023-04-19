57a7ee5c6c7dee3e949d84bf141be573c327c9d2
Luchooooo <louis.barreteau@epitech.eu>
2023-04-19 19:14:36 +0200

    feat(core): subscribe command

--- src/server/commands/subscribe_command.c
+++ src/server/commands/subscribe_command.c
@@ -13,21 +13,73 @@
 #include "teams_server.h"
 #include "data.h"
 #include "server.h"
+#include "teams_commands.h"
 
-void handle_subscribe(server_t *server, server_client_t *client, char **args)
+static bool check_in_clients_fd(server_client_t *client, int fd_tmp)
 {
-    (void)server;
-    (void)client;
-    server_client_write_string(server, client, "Command: ");
-    server_client_write_string(server, client, args[0]);
-    server_client_write_string(server, client, "\n");
-    server_client_write_string(server, client, "Arguments: ");
-    if (args[1] == NULL) {
-        server_client_write_string(server, client, "No arguments given\n");
+    if (fd_tmp == client->fd) {
+        return true;
+    }
+    return false;
+}
+
+static bool check_if_user_is_logged_in(server_client_t *client)
+{
+    user_t *tmp_user = NULL;
+
+    TAILQ_FOREACH(tmp_user, &global->users, entries) {
+        if (tmp_user && tmp_user->is_logged == true && check_in_clients_fd(
+            client, tmp_user->socket_fd)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+static bool requirement_ok(server_t *server, server_client_t *client,
+char **args)
+{
+    if (args[1] == NULL || args[2] != NULL) {
+        server_client_printf(server, client, "501 Invalid arguments\n");
+        return false;
+    }
+    if (check_if_user_is_logged_in(client) == false) {
+        server_client_printf(server, client, "430 User not logged in\n");
+        return false;
+    }
+    return true;
+}
+
+static bool check_uuid(server_t *server, server_client_t *client, char **args)
+{
+    if (is_a_uuid(args[1]) == false) {
+        server_client_printf(server, client, "550 Bad UUID\n");
+        return false;
+    }
+    return true;
+}
+
+void handle_subscribe(server_t *server, server_client_t *client,
+char **args)
+{
+    team_t *team = NULL;
+    user_t *user = NULL;
+
+    user = get_user_logged_in(client);
+    if (requirement_ok(server, client, args) == false)
+        return;
+    team = get_team_from_struct(args[1]);
+    if (team == NULL) {
+        server_client_printf(server, client, "430 Team doesn't exist\n");
         return;
     }
-    for (int i = 1; args[i]; i++) {
-        server_client_write_string(server, client, args[i]);
-        server_client_write_string(server, client, " ");
+    remove_bad_char(args[1]);
+    string_strip_delim(&args[1], '"');
+    if (check_uuid(server, client, args) == false)
+        return;
+    if (user == NULL) {
+        server_client_printf(server, client, "430 User doesn't exist\n");
+        return;
     }
+    add_user_to_team(args[1], user->user_data->uuid);
 }
